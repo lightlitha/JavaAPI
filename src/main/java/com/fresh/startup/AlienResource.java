e6473eca12a7dc2d05ef0c3048c11d7a0b546949
lightlitha <johndoe@example.com>
2021-07-21 21:22:06 +0200

    Path Params

--- src/main/java/com/fresh/startup/AlienResource.java
+++ src/main/java/com/fresh/startup/AlienResource.java
@@ -7,6 +7,7 @@ import jakarta.ws.rs.Consumes;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
+import jakarta.ws.rs.PathParam;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
 
@@ -18,9 +19,18 @@ public class AlienResource {
 	@GET
 	@Produces(MediaType.APPLICATION_JSON)
 	public List<Alien> getAliens() {
-		System.out.println("Calling getAlien function");
+		System.out.println("Calling all aliens.......");
 		
 		return repo.getAliens();
+	}
+	
+	@GET
+	@Path("alien/{id}")
+	@Produces(MediaType.APPLICATION_JSON)
+	public Alien getAlien(@PathParam("id") int id) {
+		System.out.println("Calling an alien.....");
+		
+		return repo.getAlien(id);
 	}
 	
 	@POST
@@ -28,7 +38,7 @@ public class AlienResource {
 	@Consumes(MediaType.APPLICATION_JSON)
 	@Produces({MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML})
 	public Alien createAlien(Alien a1) {
-		System.out.println("Calling createAlien function");
+		System.out.println("Inviting an alien.....");
 		System.out.println(a1);
 		repo.createAlien(a1);
 		return a1;
